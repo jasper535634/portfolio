f7c28c8ca50fadff50095e870215cfa584979efd
jasper535634 <535634@edu.rocmn.nl>
2022-01-19 11:47:16 +0100

    basic pagina gemaakt en een beetje css toegevoegt

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-hallo 
+<html>
+
+<head>
+    <link rel="stylesheet" href="media/styles.css">
+    <title>Portfolio jasper</title>
+</head>
+
+<body>
+    <header>
+        <h1 class="text">hallo dit is de header</h1>
+    </header>
+    <section>
+        <h1 class="text">hallo dit id een section
+    </section>
+        <p class="text">hallo dit de de index pagina</p>
+    </section>
+
+    <footer>
+        <p class="text"> hallo dit is de footer</p>
+    </footer>
+</body>
+
+</html>
